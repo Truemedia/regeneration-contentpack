54797c1134265bee81327e53fa18d7c78e82df38
Wade Penistone <truemedia@mediacityonline.net>
2015-02-10 20:57:12 +0000

    Generated laravel package

---
+++ src/Regeneration/Contentpack/ContentpackServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Regeneration\Contentpack;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContentpackServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		include __DIR__ . '/../../routes.php';
+		$this->package('regeneration/contentpack');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
